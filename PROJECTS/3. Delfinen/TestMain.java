351965d6d1ee2d6b2af589ecf8e4bd2c53a8add8
MadsRF <madsrunefrederiksen@gmail.com>
2018-11-22 12:25:42 +0100

    new init

---
+++ PROJECTS/3. Delfinen/TestMain.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TestMain{
+
+   public static void main(String[]args)throws InterruptedException{
+ 
+     DelfinenMenu menu = new DelfinenMenu();
+     menu.startMenu();
+
+   }
+}
